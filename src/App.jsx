194079bb19b058c3676c86574f913e4c95b80119
Rupesh Mewada <127041939+rupeshmewada@users.noreply.github.com>
2025-01-22 01:11:45 +0530

    change bg color App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -42,9 +42,9 @@ const App = () => {
 
   return (
     <div className='flex h-screen items-center'>
-      <div className='bg-gray-400 rounded shadow-xl border-gray-600 border-2 p-5'>
+      <div className='bg-gray-300 rounded shadow-xl border-gray-600 border-2 p-5'>
 
-        <div className=' h-24 text-white flex items-center justify-evenly m-auto text-4xl  '>
+        <div className=' h-24 flex items-center justify-evenly m-auto text-4xl  '>
            {clock.hour}:{clock.minute>9?clock.minute:'0'+clock.minute}:{clock.second > 9 ? clock.second : '0' + clock.second}
         </div>
         <button onClick={StartWatch} className='bg-green-500 w-auto m-2'>Start</button>
